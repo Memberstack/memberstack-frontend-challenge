822bad813c6f33730fe45110b0e729a8d8a5edcc
Anitha yesodharan <anithasarath89@gmail.com>
2021-10-07 20:51:25 +0530

    Color picker challenger code

--- packages/host/src/App.tsx
+++ packages/host/src/App.tsx
@@ -1,15 +1,36 @@
 import ColorPicker from "./components/ColorPicker";
 import ChatBotIFrame from "./components/ChatBotIFrame";
 import './App.css';
+import {useState,useEffect} from 'react';
 
 const App = () => {
+  //seting color picker value
+  const [colorPickerVal,setColorPickrState]=useState("#e66465");
+
+   //useEffect hook to use the local storage and retain that even in refresh
+  useEffect(() => {
+    const clr= localStorage.getItem('bg');
+    if (clr) {
+      document.getElementsByTagName('iframe')[0].style.background = clr;
+      setColorPickrState(clr);
+
+    }
+ }, []);
+
   const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    //setting local storage with current color
+    localStorage.setItem('bg', e.target.value); 
+
+    //finding element with tag and assign class background 
+    let frame=document.getElementsByTagName('iframe')[0];
+     frame.style.background=e.target.value?e.target.value:'#e66465';
+    setColorPickrState(e.target.value);
   };
 
   return (
     <>
       <ColorPicker
-        value="#e66465"
+        value={colorPickerVal}
         onChange={handleColorChange}
       />
       <ChatBotIFrame />
